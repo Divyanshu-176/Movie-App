1192fffe410e794280589bb8d0eddb097810f1f2
Divyanshu-176 <divyanshujain176@gmail.com>
2025-09-17 23:07:26 +0530

    movie App

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "8ffd8c98a422a18e1cc8be7de4a8e75b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
